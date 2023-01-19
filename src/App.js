50158447ec12bad5a37acaff0d77d7fe02484081
mhallihamza <hamzamhalli95@gmail.com>
2023-01-19 18:17:12 +0100

    1 change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="h-auto">
+    <div class='bg-info text-white'>
+      <h1 class='py-2 ps-4'>Contact Form</h1>
     </div>
-  );
+    <form class='w-75 ms-3 h-auto mt-3'>
+    <div class="row">
+    <label for="inputName" class="form-label fw-bold">Full Name<span class="text-danger ms-1">*</span></label>
+  <div class="col">
+    <input type="text" class="form-control" placeholder="First name" aria-label="First name" id="inputName" required></input>
+  </div>
+  <div class="col">
+    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name" id='inputName' required></input>
+  </div>
+    </div>
+    <div class='my-3'>
+      <label for="inputEmail" class="form-label fw-bold">Email<span class="text-danger ms-1">*</span></label>
+      <input type="email" class="form-control" id="inputEmail" required></input>
+    </div>
+    <div>
+      <label for="inputcomment" class="form-label fw-bold">Leave us a few words<span class="text-danger ms-1">*</span></label>
+      <textarea class="form-control" id="inputcomment"></textarea>
+    </div>
+    <div class='d-flex justify-content-center'>
+    <button class='btn btn-success mt-5'>Submit</button>
+    </div>
+    </form>
+    </div>
+  )
 }
 
 export default App;
